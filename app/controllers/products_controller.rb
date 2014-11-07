a1bf60d1458321f096c454e5e48479638a144002
Sandy Yu <throughthegrapevines@gmail.com>
2014-11-07 16:16:04 -0500

    add gem Ransack and configure Products index controller and view to incorporate searching for name contains and price range

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,10 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    # make search object
+    # :q is the default param key for search params
+    @search = Product.search(params[:q])
+    # get found item by calling result on search object
+    #f passed (distinct: true), result will generate a SELECT DISTINCT to avoid returning duplicate rows
+    @products = @search.result
   end
 end
