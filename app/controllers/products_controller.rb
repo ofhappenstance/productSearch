fcca089a5441361d9a354b85eaf7bf80d1824bfa
Sandy Yu <throughthegrapevines@gmail.com>
2014-11-09 15:20:40 -0500

    specify sort search fields

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,8 @@ class ProductsController < ApplicationController
   def index
     @search = Product.search(params[:q])
     @products = @search.result
-    # add extra dropdown that will contain all conditions aka model columns/schema
-    @search.build_condition
+    @search.build_condition if @search.conditions.empty?
+    # build sort only if empty
+    @search.build_sort if @search.sorts.empty?
   end
 end
