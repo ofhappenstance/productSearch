672b7a4a87e1df21d7502ce2924b99f6cda25b0f
Sandy Yu <throughthegrapevines@gmail.com>
2014-11-09 13:56:18 -0500

    added associations to model, added extra field so table may be searchable by product and category columns

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,8 @@
 class ProductsController < ApplicationController
   def index
-    # make search object
-    # :q is the default param key for search params
     @search = Product.search(params[:q])
-    # get found item by calling result on search object
-    #f passed (distinct: true), result will generate a SELECT DISTINCT to avoid returning duplicate rows
     @products = @search.result
+    # add extra dropdown that will contain all conditions aka model columns/schema
     @search.build_condition
   end
 end
