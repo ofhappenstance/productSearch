b9e941d314c2966f47ad3dea637ab388fb63b388
Sandy Yu <throughthegrapevines@gmail.com>
2014-11-09 14:18:21 -0500

    make search be sent trhough a POST request

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :products
+  # make POST request to solve problem of running into a limit of data that can be sent over a GET request
+  resources :products do
+    # add search routes that takes POST requests and which routes to index action
+    collection { post :search, to: 'products#index' }
+  end
   root to: 'products#index'
 end
